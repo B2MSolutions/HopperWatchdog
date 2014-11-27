dfa9661744b6c1e13a6bbcf013442a13b071db26
youhana-hana <meyouhana@gmail.com>
2014-11-27 12:54:15 +0000

    handle crashes

---
+++ HopperWatchdog/ProcessService.cs
@@ -0,0 +1,34 @@
+﻿namespace HopperWatchdog
+{
+    using OpenNETCF.ToolHelp;
+
+    public static class ProcessService
+    {
+        public static void KillProcess(string processName)
+        {
+            ProcessEntry[] processes = ProcessEntry.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (process.ExeFile.ToLower().CompareTo(processName.ToLower()) == 0)
+                {
+                    process.Kill();
+                    return;
+                }
+            }
+        }
+
+        public static bool ProcessExists(string processName)
+        {
+            ProcessEntry[] processes = ProcessEntry.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (process.ExeFile.ToLower().CompareTo(processName.ToLower()) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- HopperWatchdog/Program.cs
+++ HopperWatchdog/Program.cs
@@ -1,17 +1,15 @@
 ﻿namespace HopperWatchdog
 {
-    using System;
-    using System.Text;
-    using System.Threading;
-    using System.Diagnostics;
+    using System.Diagnostics; 
     using System.IO;
+    using System.Threading;
 
     public class Program
     {
         static void Main(string[] args)
         {
             var period = 15 * 60 * 1000;
-            var runningInteval = 5 * 60 * 1000;
+            var runningInterval = 5 * 60 * 1000;
 
             if (args.Length > 1)
             {
@@ -20,17 +18,23 @@
 
             if (args.Length > 2)
             {
-                runningInteval = int.Parse(args[1]);
+                runningInterval = int.Parse(args[1]);
             }
 
-            var timer = new Timer(Run, runningInteval, 3 * 1000, period);
+            if (ProcessService.ProcessExists("hopper.exe"))
+            {
+                return;
+            }
+
+            var timer = new Timer(Run, runningInterval, 3 * 1000, period);
             var waitHandle = new ManualResetEvent(false);
             waitHandle.WaitOne();
         }
 
-        static void Run(object runningInteval)
+        static void Run(object runningInterval)
         {
-            RunHopper(runningInteval);
+            ClearResults();
+            RunHopper(runningInterval);
             ClearResults();
         }
 
@@ -40,15 +44,40 @@
             DeleteHopperRegistry();
         }
 
-        static void RunHopper(object runningInteval)
+        static void RunHopper(object runningInterval)
         {
             var process = Process.Start(@"\windows\hopper.exe", null);
-            Thread.Sleep((int)runningInteval);
-            process.Kill();
+            Thread.Sleep((int)runningInterval);
+            EnsureHopperNotRunningAnyMore();
+        }
+
+        static void EnsureHopperNotRunningAnyMore()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+                    if (!ProcessService.ProcessExists("hopper.exe"))
+                    {
+                        return;
+                    }
+
+                    ProcessService.KillProcess("hopper.exe");
+                    Thread.Sleep(1000);
+                }
+                catch
+                {
+                }
+            }
         }
         
         static void DeleteDirectory(string path)
         {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+
             var files = Directory.GetFiles(path);
             foreach (var file in files)
             {
